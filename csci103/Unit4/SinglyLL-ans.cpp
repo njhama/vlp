14d18e63550a6005e91ac7ea51d470be00b98c44
Nicky Hama <njhama@usc.edu>
2024-03-14 00:25:52 -0700

    fix

--- csci103/Unit4/SinglyLL-ans.cpp
+++ csci103/Unit4/SinglyLL-ans.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 struct Node {
 public:
-    int data;
+    int val;
     Node* next;
-
-    Node(int data) : data(data), next(nullptr) {} // constructor for Node
+    Node(int val) : val(val), next(nullptr) {} // constructor for Node
 };
 
 // LL Class Implement
@@ -34,56 +33,97 @@ public:
     // ADD NODE TO END OF LIST
     // 1 -> 2 -> 3 becomes 1 -> 2 -> 3 -> 4 when push_back(4)
     void push_back(int data) {
+        if (head == nullptr) {
+            head = new Node(data);
+            return;
+        }   
 
+        Node * curr = head;
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+        curr->next = new Node(data);
+        curr->next->next = nullptr;
     }
 
     // ADD NODE TO FRONT OF LIST
     // 1 -> 2 -> 3 becomes 4 -> 1 -> 2 -> 3 when push_front(4)
     void push_front(int data) {
+        if (head == nullptr) {
+            head = new Node(data);
+            return;
+        }  
 
+        Node * myNode = new Node(data);
+        myNode->next = head;
+        head = myNode;
     }
 
     // return value of first node
     // 1 -> 2 -> 3 return 1
     int front() {
-
+        return head->val;
     }
 
     // return value of last node
     // 1 -> 2 -> 3 return 3
     int back() {
+        Node * curr = head;
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+        return curr->val;
 
     }
 
     // add node to the second last
     // 1 -> 2 -> 3 becomes 1 -> 2 -> 4 -> 3 when push_second_back(4)
     void push_second_back(int data) {
-
+        Node * curr = head;
+        while (curr->next->next != nullptr) {
+            curr = curr->next;
+        }
+        Node * last = curr->next;
+        curr->next = new Node(data);
+        curr->next->next = last;
     }
 
     // remove second to last node
     // 1 -> 2 -> 3 -> 4 becomes 1 -> 2 -> 4  when pop_second_back()
     void pop_second_back() {
-
+        Node * curr = head;
+        while (curr->next->next->next != nullptr) {
+            curr = curr->next;
+        }
+        Node * last = curr->next->next;
+        delete curr->next;
+        curr->next = last;
     }
 
     // add node to the one after the first
     // 1 -> 2 -> 3 becomes 1 -> 4 -> 2 -> 3 when push_second_front(4)
     void push_second_front(int data) {
-
+        Node * second = head->next;
+        head->next = new Node(data);
+        head->next->next = second;
     }
 
     // removes second node
     // 1 -> 2 -> 3 -> 4 becomes 1 -> 3 -> 4 when pop_second_front()
     void pop_second_front() {
-
+        Node * third = head->next->next;
+        delete head->next;
+        head->next = third;
     }
 
     // IGNORE (JUST TO TEST)
     void print() const {
         Node* current = head;
         while (current != nullptr) {
-            std::cout << current->data << " -> ";
+            std::cout << current->val << "";
+            if (current->next != nullptr) {
+                cout <<  " -> ";
+            }
             current = current->next;
         }
         std::cout << std::endl;
